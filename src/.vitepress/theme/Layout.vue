a1597dd4c3c16566ddb061779e9725c78b2da11c
Kevin Levron <kevin.levron@gmail.com>
2021-02-28 19:35:36 +0100

    wip

--- src/.vitepress/theme/Layout.vue
+++ src/.vitepress/theme/Layout.vue
@@ -27,16 +27,6 @@
     </Home>
     <Page v-else :class="{ examples: isExamples }" />
    
-    <ClientOnly>
-      <!-- Global site tag (gtag.js) - Google Analytics -->
-      <script async src="https://www.googletagmanager.com/gtag/js?id=UA-178028522-1"></script>
-      <script>
-        window.dataLayer = window.dataLayer || [];
-        function gtag(){dataLayer.push(arguments);}
-        gtag('js', new Date());
-        gtag('config', 'UA-178028522-1');
-      </script>
-    </ClientOnly>
   </div>
 
   <Debug />
--- src/.vitepress/theme/index.js
+++ src/.vitepress/theme/index.js
@@ -13,5 +13,29 @@ export default {
     Object.entries(Examples).forEach(([key, value]) => {
       app.component(key, value);
     });
+
+    const GA_ID = 'UA-178028522-1';
+    if (process.env.NODE_ENV === 'production' && typeof window !== 'undefined') {
+      (function (i, s, o, g, r, a, m) {
+        i['GoogleAnalyticsObject'] = r
+        i[r] = i[r] || function () {
+          (i[r].q = i[r].q || []).push(arguments)
+        }
+        i[r].l = 1 * new Date()
+        a = s.createElement(o)
+        m = s.getElementsByTagName(o)[0]
+        a.async = 1
+        a.src = g
+        m.parentNode.insertBefore(a, m)
+      })(window, document, 'script', 'https://www.google-analytics.com/analytics.js', 'ga')
+  
+      ga('create', GA_ID, 'auto')
+      ga('set', 'anonymizeIp', true)
+  
+      router.afterEach(function (to) {
+        ga('set', 'page', router.app.$withBase(to.fullPath))
+        ga('send', 'pageview')
+      })
+    }
   }
 };
