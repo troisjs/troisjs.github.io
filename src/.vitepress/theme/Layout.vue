416bf7910cfd80010fe9e92ed8b0225c8b945966
Kevin Levron <kevin.levron@gmail.com>
2021-02-28 19:17:56 +0100

    wip

--- src/.vitepress/theme/Layout.vue
+++ src/.vitepress/theme/Layout.vue
@@ -26,6 +26,15 @@
       </template>
     </Home>
     <Page v-else :class="{ examples: isExamples }" />
+   
+    <!-- Global site tag (gtag.js) - Google Analytics -->
+    <script async src="https://www.googletagmanager.com/gtag/js?id=UA-178028522-1"></script>
+    <script>
+      window.dataLayer = window.dataLayer || [];
+      function gtag(){dataLayer.push(arguments);}
+      gtag('js', new Date());
+      gtag('config', 'UA-178028522-1');
+    </script>
   </div>
 
   <Debug />
