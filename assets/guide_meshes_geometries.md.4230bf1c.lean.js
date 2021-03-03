4129b37a5c9ee730d2d13c829fa8550aecbcc567
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2021-03-03 22:53:11 +0000

    Deploy troisjs/troisjs.github.io to github.com/troisjs/troisjs.github.io.git:gh-pages

---
+++ assets/guide_meshes_geometries.md.4230bf1c.lean.js
@@ -0,0 +1,1 @@
+import{o as e,c as r,d as o}from"./app.39908f78.js";const t='{"title":"Using Geometries","description":"","frontmatter":{},"relativePath":"guide/meshes/geometries.md","lastUpdated":1614811945208}',s={},a=o('',5);s.render=function(o,t,s,n,i,c){return e(),r("div",null,[a])};export default s;export{t as __pageData};
