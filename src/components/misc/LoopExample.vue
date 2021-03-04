9532892768f925116399b94fad1dfbbe3405b286
Kevin Levron <kevin.levron@gmail.com>
2021-03-04 13:04:24 +0100

    update loop example

--- src/components/misc/LoopExample.vue
+++ src/components/misc/LoopExample.vue
@@ -1,63 +1,50 @@
 <template>
-  <!-- <div> -->
-    <!-- <div class="info">v-for loop example</div> -->
-    <Renderer ref="renderer" orbit-ctrl @click="onClick">
-      <Camera :position="{ z: 15 }" />
-      <Scene>
-        <PointLight color="#ffffff" :position="{ y: 50, z: 0 }" />
-        <PointLight color="#ffffff" :position="{ y: -50, z: 0 }" />
-        <PointLight color="#ffffff" :position="{ y: 0, z: 0 }" />
-        <Torus
-          v-for="i in n" :key="i" :ref="`mesh${i}`"
-          :radius="i * 0.2"
-          :tube="0.05"
-          :radial-segments="8"
-          :tubular-segments="(i+2) * 3"
-        >
-          <StandardMaterial :color="color(i)" />
-        </Torus>
-      </Scene>
-      <EffectComposer>
-        <RenderPass />
-        <UnrealBloomPass :strength="1" />
-        <FXAAPass />
-      </EffectComposer>
-    </Renderer>
-  <!-- </div> -->
+  <Renderer ref="renderer" antialias orbit-ctrl @click="onClick">
+    <Camera :position="{ z: 15 }" />
+    <Scene background="#ffffff">
+      <AmbientLight color="#808080" />
+      <PointLight color="#ffffff" :position="{ y: 50, z: 0 }" />
+      <PointLight color="#ffffff" :position="{ y: -50, z: 0 }" />
+      <PointLight color="#ffffff" :position="{ y: 0, z: 0 }" />
+      <Torus
+        v-for="i in n" :key="i" :ref="`mesh${i}`"
+        :radius="i * 0.2"
+        :tube="0.1"
+        :radial-segments="8"
+        :tubular-segments="(i+2) * 4"
+      >
+        <ToonMaterial :color="color(i)" />
+      </Torus>
+    </Scene>
+  </Renderer>
 </template>
 
 <script>
 import {
+  AmbientLight,
   Camera,
-  EffectComposer,
-  FXAAPass,
   PointLight,
   Renderer,
-  RenderPass,
   Scene,
-  StandardMaterial,
+  ToonMaterial,
   Torus,
-  UnrealBloomPass,
 } from 'troisjs';
 
 import chroma from 'chroma-js';
 
 export default {
   components: {
+    AmbientLight,
     Camera,
-    EffectComposer,
-    FXAAPass,
     PointLight,
     Renderer,
-    RenderPass,
     Scene,
-    StandardMaterial,
+    ToonMaterial,
     Torus,
-    UnrealBloomPass,
   },
   data() {
     return {
-      n: 50,
+      n: 30,
       cscale: chroma.scale(['#dd3e1b', '#0b509c']),
     };
   },
@@ -80,7 +67,7 @@ export default {
       return this.cscale(i / this.n).css();
     },
     onClick(e) {
-      this.cscale = chroma.scale([chroma.random(), chroma.random()]);
+      this.cscale = chroma.scale([chroma.random(), chroma.random(), chroma.random()]);
     },
   },
 };
