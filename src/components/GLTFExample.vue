b1ed67a97484c773eb4e6e6f45f72ec01ceec03e
Kevin Levron <kevin.levron@gmail.com>
2021-03-04 01:24:55 +0100

    cleanup [skip ci]

--- src/components/GLTFExample.vue
+++
@@ -1,9 +0,0 @@
-<template>
-  <GLTFViewer src="/assets/test.glb" :camera-position="{ z: 1 }">
-    <AmbientLight></AmbientLight>
-  </GLTFViewer>
-</template>
-
-<script>
-export default {};
-</script>
