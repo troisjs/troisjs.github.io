66d1f7bfc1e978fa2fe634fc8ace9e1d83e6eb67
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2021-03-10 22:24:10 +0000

    Deploy troisjs/troisjs.github.io to github.com/troisjs/troisjs.github.io.git:gh-pages

--- assets/guide_hmr.md.1f0990e6.lean.js
+++
@@ -1,1 +0,0 @@
-import{o as e,c as t,d as i}from"./app.704efae6.js";const l='{"title":"HMR","description":"","frontmatter":{},"relativePath":"guide/hmr.md","lastUpdated":1615405782205}',a={},s=i('',3);a.render=function(i,l,a,r,o,n){return e(),t("div",null,[s])};export default a;export{l as __pageData};
---
+++ assets/guide_materials_lambert-material.md.79ec7344.lean.js
@@ -0,0 +1,1 @@
+import{o as a,c as t,d as e}from"./app.b14967d2.js";const s='{"title":"LambertMaterial","description":"","frontmatter":{},"headers":[{"level":3,"title":"Props","slug":"props"}],"relativePath":"guide/materials/lambert-material.md","lastUpdated":1615415005089}',r={},n=e('',7);r.render=function(e,s,r,p,o,l){return a(),t("div",null,[n])};export default r;export{s as __pageData};
