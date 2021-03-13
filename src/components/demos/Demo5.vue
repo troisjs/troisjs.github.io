8c1d7f747ab102760045f36ce38460bc6b389072
Kevin Levron <kevin.levron@gmail.com>
2021-03-13 22:51:47 +0100

    re reorder...

--- src/components/demos/Demo5.vue
+++ src/components/demos/Demo5.vue
@@ -1,25 +1,129 @@
 <template>
-  <Renderer ref="renderer" resize :orbit-ctrl="{ enableDamping: true, dampingFactor: 0.05 }">
-    <Camera :position="{ x: 0, y: 0, z: 100 }" />
-    <Scene background="#000000">
-      <NoisyImage
-        src="/assets/images/img2.jpg"
-        :width="800"
-        :time-coef="0.001"
-        :noise-coef="2"
-        :z-coef="5"
-        :disp-coef="0.015"
-        :rotation="{ x: -Math.PI / 6 }"
-      />
+  <Renderer ref="renderer" resize mouse-move @mousemove="updateTilt">
+    <Camera :position="{ y: -20, z: 10 }" :look-at="{ x: 0, y: 0, z: 0 }" />
+    <Scene background="#ffffff">
+      <AmbientLight />
+      <PointLight ref="light" :position="{ y: 0, z: 20 }" />
+      <InstancedMesh ref="imesh" :count="NUM_INSTANCES" :position="{ y: 20, z: 10 }">
+        <BoxGeometry :size="SIZE" />
+        <PhongMaterial vertex-colors />
+      </InstancedMesh>
     </Scene>
+    <EffectComposer>
+      <RenderPass />
+      <FXAAPass />
+      <TiltShiftPass :gradient-radius="tiltRadius" :start="{ x: 0, y: this.tiltY }" :end="{ x: 100, y: this.tiltY }" />
+    </EffectComposer>
   </Renderer>
 </template>
 
 <script>
-import { Camera, Renderer, Scene } from 'troisjs';
-import NoisyImage from 'troisjs/src/components/noisy/NoisyImage.js';
+import { InstancedBufferAttribute, Object3D } from 'three';
+import {
+  AmbientLight,
+  BoxGeometry,
+  Camera,
+  EffectComposer,
+  FXAAPass,
+  InstancedMesh,
+  PhongMaterial,
+  PointLight,
+  Renderer,
+  RenderPass,
+  Scene,
+  TiltShiftPass,
+} from 'troisjs';
+import SimplexNoise from 'simplex-noise';
+
+const simplex = new SimplexNoise();
 
 export default {
-  components: { Camera, NoisyImage, Renderer, Scene },
+  components: {
+    AmbientLight,
+    BoxGeometry,
+    Camera,
+    EffectComposer,
+    FXAAPass,
+    InstancedMesh,
+    PhongMaterial,
+    PointLight,
+    Renderer,
+    RenderPass,
+    Scene,
+    TiltShiftPass,
+  },
+  setup() {
+    const SIZE = 1.6, NX = 25, NY = 25, PADDING = 1;
+    const SIZEP = SIZE + PADDING;
+    const W = NX * SIZEP - PADDING;
+    const H = NY * SIZEP - PADDING;
+    return {
+      SIZE, NX, NY, PADDING,
+      SIZEP, W, H,
+      NUM_INSTANCES: NX * NY,
+    };
+  },
+  data() {
+    return {
+      tiltRadius: 100,
+      tiltY: 100,
+    };
+  },
+  computed: {
+    tiltStart() { return { x: 0, y: this.tiltY }; },
+    tiltEnd() { return { x: 100, y: this.tiltY }; },
+  },
+  mounted() {
+    this.renderer = this.$refs.renderer;
+    this.size = this.renderer.three.size;
+    this.mouse = this.renderer.three.mouse;
+    this.imesh = this.$refs.imesh.mesh;
+
+    // init color attribute
+    const colors = [];
+    for (let i = 0; i < this.NUM_INSTANCES; i++) {
+      const c = Math.random();
+      colors.push(c, c, c);
+    }
+    this.imesh.geometry.setAttribute('color', new InstancedBufferAttribute(new Float32Array(colors), 3));
+
+    this.tiltRadius = this.size.height / 3;
+    this.tiltY = this.size.height / 2;
+    this.renderer.onAfterResize(this.updateTilt);
+
+    this.dummy = new Object3D();
+    this.renderer.onBeforeRender(this.animate);
+  },
+  methods: {
+    animate() {
+      this.updateInstanceMatrix();
+    },
+    updateTilt() {
+      this.tiltRadius = this.size.height / 3;
+      this.tiltY = (this.mouse.y + 1) * 0.5 * this.size.height;
+    },
+    updateInstanceMatrix() {
+      const x0 = -this.W / 2 + this.PADDING;
+      const y0 = -this.H / 2 + this.PADDING;
+      const time = Date.now() * 0.0001;
+      const noise = 0.005;
+      let x, y, nx, ny, rx, ry;
+      for (let i = 0; i < this.NX; i++) {
+        for (let j = 0; j < this.NY; j++) {
+          x = x0 + i * this.SIZEP;
+          y = y0 + j * this.SIZEP;
+          nx = x * noise;
+          ny = y * noise;
+          rx = simplex.noise3D(nx, ny, time) * Math.PI;
+          ry = simplex.noise3D(ny, nx, time) * Math.PI;
+          this.dummy.position.set(x, y, -10);
+          this.dummy.rotation.set(rx, ry, 0);
+          this.dummy.updateMatrix();
+          this.imesh.setMatrixAt(i * this.NY + j, this.dummy.matrix);
+        }
+      }
+      this.imesh.instanceMatrix.needsUpdate = true;
+    },
+  },
 };
 </script>
--- src/guide/index.md
+++ src/guide/index.md
@@ -5,12 +5,12 @@
 <p>
   <a href="/examples/demos/3"><img src="/assets/screenshots/troisjs_1.jpg" style="width:30%; margin-right: 1%;" /></a>
   <a href="/examples/demos/4"><img src="/assets/screenshots/troisjs_3.jpg" style="width:30%; margin-right: 1%;" /></a>
-  <a href="/examples/demos/6"><img src="/assets/screenshots/troisjs_4.jpg" style="width:30%; margin-right: 1%;" /></a>
-  <a href="/examples/demos/5"><img src="/assets/screenshots/troisjs_5.jpg" style="width:30%; margin-right: 1%;" /></a>
+  <a href="/examples/demos/1"><img src="/assets/screenshots/troisjs_4.jpg" style="width:30%; margin-right: 1%;" /></a>
+  <a href="/examples/demos/2"><img src="/assets/screenshots/troisjs_5.jpg" style="width:30%; margin-right: 1%;" /></a>
   <a href="/examples/materials/2"><img src="/assets/screenshots/troisjs_2.jpg" style="width:30%; margin-right: 1%;" /></a>
   <a href="/examples/loop"><img src="/assets/screenshots/troisjs_6.jpg" style="width:30%; margin-right: 1%;" /></a>
   <a href="/examples/shadows"><img src="/assets/screenshots/troisjs_7.jpg" style="width:30%; margin-right: 1%;" /></a>
-  <a href="/examples/demos/1"><img src="/assets/screenshots/troisjs_8.jpg" style="width:30%; margin-right: 1%;" /></a>
+  <a href="/examples/demos/5"><img src="/assets/screenshots/troisjs_8.jpg" style="width:30%; margin-right: 1%;" /></a>
   <a href="/examples/lights"><img src="/assets/screenshots/troisjs_9.jpg" style="width:30%; margin-right: 1%;" /></a>
 </p>
 
