6e0e21f93d33a5e5cb59f0ec8c5c466c3cf37934
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2021-04-05 21:15:48 +0000

    Deploy troisjs/troisjs.github.io to github.com/troisjs/troisjs.github.io.git:gh-pages

--- assets/guide_materials_standard-material.md.d8b2ef8c.lean.js
+++
@@ -1,1 +0,0 @@
-import{o as t,c as e,d as a}from"./app.80aaf375.js";const d='{"title":"StandardMaterial","description":"","frontmatter":{},"headers":[{"level":3,"title":"Props","slug":"props"}],"relativePath":"guide/materials/standard-material.md","lastUpdated":1617567470056}',r={},o=a('',7);r.render=function(a,d,r,s,n,c){return t(),e("div",null,[o])};export default r;export{d as __pageData};
--- assets/guide_usage_raf.md.3f5d1a1b.lean.js
+++
@@ -1,1 +0,0 @@
-import{o as n,c as a,d as s}from"./app.80aaf375.js";const t='{"title":"Render loop","description":"","frontmatter":{},"relativePath":"guide/usage/raf.md","lastUpdated":1617567470060}',p={},o=s('',4);p.render=function(s,t,p,e,c,u){return n(),a("div",null,[o])};export default p;export{t as __pageData};
