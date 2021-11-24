4f318e96966b0738e6bdbd9797a2180c41a98cfc
Kevin Levron <kevin.levron@gmail.com>
2021-11-24 12:01:38 +0100

    demo7 script setup

--- src/components/demos/Demo7.vue
+++ src/components/demos/Demo7.vue
@@ -1,95 +1,69 @@
 <template>
-  <Renderer ref="renderer" antialias :pointer="{ onMove: onPointerMove }" resize :orbit-ctrl="{ enableDamping: true }">
-    <Camera :position="{ x: 0, y: 0, z: 20 }" />
-    <Scene ref="scene">
+  <Renderer ref="rendererRef" antialias :pointer="{ onMove: onPointerMove }" resize :orbit-ctrl="{ enableDamping: true }">
+    <Camera :position="{ z: 20 }" />
+    <Scene>
       <AmbientLight />
       <PointLight :color="light1Color" :position="{ x: 50, y: 50, z: 50}" />
       <PointLight :color="light2Color" :position="{ x: -50, y: 50, z: 50}" />
       <PointLight :color="light3Color" :position="{ x: -50, y: -50, z: 50}" />
       <PointLight :color="light4Color" :position="{ x: 50, y: -50, z: 50}" />
-
-      <LiquidPlane ref="liquid"
-        :width="WIDTH" :height="HEIGHT" :width-segments="512" :height-segments="512"
-        :material-props="{ color, metalness, roughness }"
+      <LiquidPlane ref="liquidRef"
+        :width="WIDTH" :height="HEIGHT"
+        :width-segments="512" :height-segments="512"
+        :material-props="materialProps"
       />
     </Scene>
   </Renderer>
 </template>
 
-<script>
-import { Plane, Raycaster, Vector3 } from 'three';
-import { Pane } from 'tweakpane';
-import chroma from 'chroma-js';
+<script setup>
+import { onUnmounted, ref } from 'vue'
+import { Plane, Raycaster, Vector3 } from 'three'
+import { Pane } from 'tweakpane'
+import chroma from 'chroma-js'
 
-import {
-  AmbientLight,
-  Camera,
-  PointLight,
-  Renderer,
-  Scene,
-} from 'troisjs';
+import { AmbientLight, Camera, PointLight, Renderer, Scene } from 'troisjs'
+import LiquidPlane from '@troisjs/components/src/liquid/LiquidPlane.js'
 
-import LiquidPlane from '@troisjs/components/src/liquid/LiquidPlane.js';
+const WIDTH = 30
+const HEIGHT = 30
 
-export default {
-  components: {
-    AmbientLight,
-    Camera,
-    LiquidPlane,
-    PointLight,
-    Renderer,
-    Scene,
-  },
-  setup() {
-    return {
-      WIDTH: 30,
-      HEIGHT: 30,
-    };
-  },
-  data() {
-    return {
-      color: '#ffffff',
-      metalness: 1,
-      roughness: 0.2,
-      light1Color: '#FFFF80',
-      light2Color: '#DE3578',
-      light3Color: '#FF4040',
-      light4Color: '#0d25bb',
-    };
-  },
-  mounted() {
-    this.pointer = this.$refs.renderer.three.pointer;
-    this.liquidEffect = this.$refs.liquid.liquidEffect;
-    this.liquidEffect.addDrop(0, 0, 0.05, 0.05);
+const materialProps = ref({
+  color: '#ffffff',
+  metalness: 1,
+  roughness: 0.2
+})
 
-    // custom raycaster
-    this.raycaster = new Raycaster();
-    this.pointerPlane = new Plane(new Vector3(0, 0, 1), 0);
-    this.pointerV3 = new Vector3();
+const light1Color = ref('#FFFF80')
+const light2Color = ref('#DE3578')
+const light3Color = ref('#FF4040')
+const light4Color = ref('#0d25bb')
 
-    this.pane = new Pane();
-    this.pane.addInput(this, 'color');
-    this.pane.addInput(this, 'metalness', { min: 0, max: 1 });
-    this.pane.addInput(this, 'roughness', { min: 0, max: 1 });
-    this.pane.addButton({ title: 'Random lights' }).on('click', this.randomColors);
-  },
-  unmounted() {
-    this.pane.dispose();
-  },
-  methods: {
-    onPointerMove() {
-      this.raycaster.setFromCamera(this.pointer.positionN, this.$refs.renderer.three.camera);
-      this.raycaster.ray.intersectPlane(this.pointerPlane, this.pointerV3);
-      const x = 2 * this.pointerV3.x / this.WIDTH;
-      const y = 2 * this.pointerV3.y / this.HEIGHT;
-      this.liquidEffect.addDrop(x, y, 0.025, 0.005);
-    },
-    randomColors() {
-      this.light1Color = chroma.random().hex();
-      this.light2Color = chroma.random().hex();
-      this.light3Color = chroma.random().hex();
-      this.light4Color = chroma.random().hex();
-    },
-  },
-};
+// custom raycaster
+const raycaster = new Raycaster()
+const pointerPlane = new Plane(new Vector3(0, 0, 1), 0)
+const pointerV3 = new Vector3()
+
+const pane = new Pane()
+onUnmounted(() => { pane.dispose() })
+pane.addInput(materialProps.value, 'color')
+pane.addInput(materialProps.value, 'metalness', { min: 0, max: 1 })
+pane.addInput(materialProps.value, 'roughness', { min: 0, max: 1 })
+pane.addButton({ title: 'Random lights' }).on('click', () => {
+  light1Color.value = chroma.random().hex()
+  light2Color.value = chroma.random().hex()
+  light3Color.value = chroma.random().hex()
+  light4Color.value = chroma.random().hex()
+})
+
+const rendererRef = ref()
+const liquidRef = ref()
+
+function onPointerMove({ positionN }) {
+  raycaster.setFromCamera(positionN, rendererRef.value.camera)
+  raycaster.ray.intersectPlane(pointerPlane, pointerV3)
+  const x = 2 * pointerV3.x / WIDTH
+  const y = 2 * pointerV3.y / HEIGHT
+  liquidRef.value.liquidEffect.addDrop(x, y, 0.025, 0.005)
+}
 </script>
